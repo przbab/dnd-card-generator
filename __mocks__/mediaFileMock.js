88d624fb0e42b99e4d22f870cac91d6a9ea81b9c
Przemysław Babiarz <przemyslaw.babiarz@schibsted.pl>
2022-09-18 14:53:51 +0200

    chore: initial commit

---
+++ __mocks__/mediaFileMock.js
@@ -0,0 +1,1 @@
+module.exports = { src: 'mocked-media-file-src', width: 100, height: 100, blurDataURL: 'mocked-media-file-blur' };
---
+++ next.config.js
@@ -0,0 +1,67 @@
+const nextConfig = {
+    experimental: {
+        scrollRestoration: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    publicRuntimeConfig: {},
+    serverRuntimeConfig: {},
+    compress: false,
+    poweredByHeader: false,
+    images: {
+        formats: ['image/avif', 'image/webp'],
+    },
+    async headers() {
+        const globalHeaders = [
+            {
+                key: 'Cache-Control',
+                value: "600",
+            },
+            {
+                key: 'X-Content-Type-Options',
+                value: 'nosniff',
+            },
+            {
+                key: 'X-DNS-Prefetch-Control',
+                value: 'off',
+            },
+            {
+                key: 'X-Download-Options',
+                value: 'noopen',
+            },
+            {
+                key: 'X-Frame-Options',
+                value: 'sameorigin',
+            },
+            {
+                key: 'X-Permitted-Cross-Domain-Policies',
+                value: 'none',
+            },
+            {
+                key: 'X-XSS-Protection',
+                value: '0',
+            },
+        ];
+
+        if (process.env.NODE_ENV !== 'development') {
+            globalHeaders.push({
+                key: 'Strict-Transport-Security',
+                value: 'max-age=63072000; preload',
+            });
+        }
+
+        return [
+            {
+                source: '/',
+                headers: globalHeaders,
+            },
+            {
+                source: '/:path*',
+                headers: globalHeaders,
+            },
+        ];
+    }
+};
+
+module.exports = nextConfig;
