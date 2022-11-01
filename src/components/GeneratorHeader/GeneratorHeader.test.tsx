92b63e0212417628041974273207188121ad307f
Przemysław Babiarz <przbab3@gmail.com>
2022-11-01 15:54:01 +0100

    chore: remove empty test

--- src/components/GeneratorHeader/GeneratorHeader.test.tsx
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-import GeneratorHeader from './GeneratorHeader';
-
-describe('Components', () => {
-    describe('GeneratorHeader', () => {
-        test.todo('Should switch generator tabs');
-    });
-});
