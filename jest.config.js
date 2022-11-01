01cb496230dfc00670cb82b07073cd9762e98d29
Przemysław Babiarz <przbab3@gmail.com>
2022-11-01 10:35:23 +0100

    chore: ignore .github directory

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,5 @@ module.exports = {
         '\\.(gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
         '\\.(jpg|jpeg|png)$': '<rootDir>/__mocks__/mediaFileMock.js',
     },
-    testPathIgnorePatterns: ['/node_modules/'],
+    testPathIgnorePatterns: ['/node_modules/', '/.github/', '/.next/'],
 };
