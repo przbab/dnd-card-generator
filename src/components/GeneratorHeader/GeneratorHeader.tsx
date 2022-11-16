563a8b52317f1625f16551113b0d7b1bd3db7397
Wojciech Pająk <arachnomorph@gmail.com>
2022-11-16 18:05:48 +0100

    chore: code review fixes

--- src/components/GeneratorHeader/GeneratorHeader.tsx
+++ src/components/GeneratorHeader/GeneratorHeader.tsx
@@ -1,13 +1,18 @@
 import Link from 'next/link';
 import React from 'react';
 import { linkToGenerator, linkToSearch } from '../../helpers/links';
+import styles from './GeneratorHeader.module.scss';
 
 function GeneratorHeader() {
     return (
         <header>
-            <nav>
-                <Link href={linkToGenerator()}>Generator</Link>
-                <Link href={linkToSearch()}>Search</Link>
+            <nav className={styles.root}>
+                <Link className={[styles.activeTab, styles.tab].join(' ')} href={linkToGenerator()}>
+                    Generator
+                </Link>
+                <Link className={[styles.inactiveTab, styles.tab].join(' ')} href={linkToSearch()}>
+                    Search
+                </Link>
             </nav>
         </header>
     );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,11 @@ import '../styles/global.scss';
 import type { ReactNode } from 'react';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
+import { Roboto } from '@next/font/google';
+
+const roboto = Roboto({
+    weight: ['100', '400', '700'],
+});
 
 export type NextPageWithLayout<P = unknown, IP = P> = NextPage<P, IP> & {
     getLayout?: () => ReactNode;
@@ -16,7 +21,7 @@ function DnDCardGenerator({ Component, pageProps }: AppPropsWithLayout) {
     const getLayout = Component.getLayout ?? ((page) => page);
 
     // eslint-disable-next-line react/jsx-props-no-spreading
-    return getLayout(<Component {...pageProps} />);
+    return getLayout(<Component className={roboto.className} {...pageProps} />);
 }
 
 export default DnDCardGenerator;
