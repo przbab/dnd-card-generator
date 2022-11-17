4ad49c1ad8efb4a4d6b7473cb9239505004ef899
Wojciech Pająk <arachnomorph@gmail.com>
2022-11-17 19:56:31 +0100

    chore: code review fixes

--- src/components/GeneratorHeader/GeneratorHeader.tsx
+++ src/components/GeneratorHeader/GeneratorHeader.tsx
@@ -1,16 +1,26 @@
 import Link from 'next/link';
 import React from 'react';
+import cn from 'classnames';
+import { useRouter } from 'next/router';
 import { linkToGenerator, linkToSearch } from '../../helpers/links';
 import styles from './GeneratorHeader.module.scss';
 
 function GeneratorHeader() {
+    const router = useRouter();
+
     return (
         <header>
             <nav className={styles.root}>
-                <Link className={[styles.activeTab, styles.tab].join(' ')} href={linkToGenerator()}>
+                <Link
+                    className={cn(router.asPath.includes('search') ? styles.inactiveTab : styles.activeTab, styles.tab)}
+                    href={linkToGenerator()}
+                >
                     Generator
                 </Link>
-                <Link className={[styles.inactiveTab, styles.tab].join(' ')} href={linkToSearch()}>
+                <Link
+                    className={cn(router.asPath.includes('search') ? styles.activeTab : styles.inactiveTab, styles.tab)}
+                    href={linkToSearch()}
+                >
                     Search
                 </Link>
             </nav>
