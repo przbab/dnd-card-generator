eabe9c7320147fe8d582867c52d6efd5cfd50a32
Wojciech Pająk <88026038+Arachnomorph@users.noreply.github.com>
2022-11-17 21:14:26 +0100

    feat: header styling (#5)
    * feat: header styling
    
    * chore: code review fixes
    
    * chore: code review fixes
    
    * chore: code review fixes
    
    * chore: code review fixes

--- src/components/GeneratorHeader/GeneratorHeader.tsx
+++ src/components/GeneratorHeader/GeneratorHeader.tsx
@@ -1,13 +1,34 @@
 import Link from 'next/link';
 import React from 'react';
+import cn from 'classnames';
+import { useRouter } from 'next/router';
 import { linkToGenerator, linkToSearch } from '../../helpers/links';
+import styles from './GeneratorHeader.module.scss';
 
 function GeneratorHeader() {
+    const router = useRouter();
+
     return (
         <header>
-            <nav>
-                <Link href={linkToGenerator()}>Generator</Link>
-                <Link href={linkToSearch()}>Search</Link>
+            <nav className={styles.root}>
+                <Link
+                    className={cn(
+                        styles.tab,
+                        router.pathname === linkToGenerator() ? styles.activeTab : styles.inactiveTab
+                    )}
+                    href={linkToGenerator()}
+                >
+                    Generator
+                </Link>
+                <Link
+                    className={cn(
+                        styles.tab,
+                        router.pathname === linkToSearch() ? styles.activeTab : styles.inactiveTab
+                    )}
+                    href={linkToSearch()}
+                >
+                    Search
+                </Link>
             </nav>
         </header>
     );
